2704e4b7759f41738224dfb1c00a18fd882248ca
David Gardiner <david@gardiner.net.au>
2016-03-01 09:59:11 +1030

    Initial commit

---
+++ src/tools/chocolateyinstall.ps1
@@ -0,0 +1,22 @@
+﻿$ErrorActionPreference = 'Stop';
+
+
+$packageName= 'SQL Server Management Studio'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'http://download.microsoft.com/download/B/9/0/B9084A6E-18C7-454E-AA05-5F375B9A3638/SSMS-Full-Setup.exe'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+
+  silentArgs    = "/quiet /install /norestart /log `"$env:TEMP\chocolatey\$($packageName)\$($packageName).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+
+  softwareName  = 'SQL Server Management Studio - January 2016'
+  checksum      = '091CB95E1B87453B855A5C387137AEB4'
+  checksumType  = 'md5'
+}
+
+Install-ChocolateyPackage @packageArgs
