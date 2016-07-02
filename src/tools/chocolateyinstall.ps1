de3f2c0dd06907ed568835a6eed2e85515db3954
David Gardiner <david@gardiner.net.au>
2016-07-02 13:32:19 +0930

    July release - 13.0.15500.91
    Improved release notes formatting

--- src/tools/chocolateyinstall.ps1
+++ src/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@
 
 $packageName= 'SQL Server Management Studio'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'http://download.microsoft.com/download/E/D/3/ED3B06EC-E4B5-40B3-B861-996B710A540C/SSMS-Setup-ENU.exe'
+$url = 'http://download.microsoft.com/download/6/F/C/6FCFDC7F-772F-4FEF-BD48-D75C9A3CFB54/SSMS-Setup-ENU.exe'
 
 $packageArgs = @{
   packageName   = $packageName
@@ -15,7 +15,7 @@ $packageArgs = @{
   validExitCodes= @(0, 3010, 1641)
 
   softwareName  = 'SQL Server Management Studio*'
-  checksum      = '5DE280315BCBCB91A70064794FE827D5'
+  checksum      = 'E00897FEC547B6112A2037AC6C37FE75'
   checksumType  = 'md5'
 }
 
