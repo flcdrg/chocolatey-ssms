4ffb8977ef484e75c4428beabc61e19b0233531e
David Gardiner <david@gardiner.net.au>
2016-07-02 22:18:58 +0930

    Add check on Win8.1/2012R2 for prerequisite KB

--- src/tools/chocolateyinstall.ps1
+++ src/tools/chocolateyinstall.ps1
@@ -1,6 +1,17 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 
+(Get-WmiObject -Class Win32_OperatingSystem).Version -match "(?<Major>\d+).(?<Minor>\d+).(?<Build>\d+)" | Out-Null
+
+if ($Matches.Major -eq 6 -and $Matches.Minor -eq 3)
+{
+    # Windows 8.1 / Server 2012 R2 requires a prerequisite hotfix 
+    if (-not (Get-HotFix -Id KB2919355 -ErrorAction SilentlyContinue))
+    {
+        Write-Error "A prerequisite for installing SQL 2016 on Windows 8.1 and Windows Server 2012 R2 is to have hotfix KB2919355 installed. See https://msdn.microsoft.com/library/ms143506.aspx for more details"
+    }
+}
+
 $packageName= 'SQL Server Management Studio'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url = 'http://download.microsoft.com/download/6/F/C/6FCFDC7F-772F-4FEF-BD48-D75C9A3CFB54/SSMS-Setup-ENU.exe'
